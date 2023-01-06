091547c9b9e9caf5e576a71f0af0f1ffd836494c
Phanor Coll <phanorcoll@gmail.com>
2023-01-06 17:23:58 -0400

    🗃️ Connection to Mongodb
    Installed the Mongoose and @nestjs/mongoose packages to connect
    to the specify database 'vertrical'.
    Also configured the app.module to start the connection.
    
    ✏️ close #4

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,19 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { EmailsModule } from './emails/emails.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [UsersModule, EmailsModule],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb://root:example@0.0.0.0:27017',
+      {
+        dbName: 'vertrical'
+      }
+    ),
+    UsersModule,
+    EmailsModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
