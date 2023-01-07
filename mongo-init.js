d2ba2f286bb7f548dcdacd8fcfb106b7d1a97ee2
Phanor Coll <phanorcoll@gmail.com>
2023-01-07 11:11:08 -0400

    👽️ Updated seed data
    Change structure of the seed data, the relation between users and emails will be by the user's email,
    on a different collection.
    
    ✏️ related to #3

--- mongo-init.js
+++ mongo-init.js
@@ -13,37 +13,12 @@ db.users.insertMany([
   {
     "name": "Phanor",
     "email": "hello@test.com",
-    "receivedEmails":[
-      {
-        "from": "pc@hotmail.com",
-        "receivedDate": new Date(),
-        "read": false,
-        "deleted": false,
-        "body": "Nulla facilisi cras fermentum odio eu feugiat. Elit pellentesque habitant morbi tristique. Libero justo laoreet sit amet. Porttitor eget dolor morbi non arcu risus quis.",
-      }
-    ],
     "createdAt": new Date("2023-01-06T22:26:31.214+00:00"),
     "updatedAt": new Date("2023-01-06T22:26:31.214+00:00")
   },
   {
     "name": "John",
     "email": "test@test.com",
-    "receivedEmails":[
-      {
-        "from": "pcomputer@gmail.com",
-        "receivedDate": new Date(),
-        "read": false,
-        "deleted": false,
-        "body": "Nulla facilisi cras fermentum odio eu feugiat. Elit pellentesque habitant morbi tristique. Libero justo laoreet sit amet. Porttitor eget dolor morbi non arcu risus quis. At tellus at urna condimentum mattis pellentesque id nibh tortor.",
-      },
-      {
-        "from": "pc@hotmail.com",
-        "receivedDate": new Date(),
-        "read": false,
-        "deleted": false,
-        "body": "Nulla facilisi cras fermentum odio eu feugiat. Elit pellentesque habitant morbi tristique. Libero justo laoreet sit amet. Porttitor eget dolor morbi non arcu risus quis.",
-      }
-    ],
     "createdAt": new Date("2023-01-06T22:26:31.214+00:00"),
     "updatedAt": new Date("2023-01-06T22:26:31.214+00:00")
   },
