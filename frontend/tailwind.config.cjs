10f209ff96b3ad67c2e310530c5ab94dd16d615b
Phanor Coll <phanorcoll@gmail.com>
2023-01-12 10:02:32 -0400

    🎨 Styles Login component
    Added styles to the login component.
    
    ✏️ related to #16

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -1,10 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,js}"
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        roboto: ['Roboto', 'sans-serif'],
+        sans: ['Roboto'],
+      },
+    },
   },
   plugins: [],
 };
